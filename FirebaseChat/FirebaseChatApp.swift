d554291c1d7510a950ed5877f3e4d2d4580a5c21
Ryu-nakay-from_MBA <70783066+Ryu-nakay@users.noreply.github.com>
2022-05-07 17:09:17 +0900

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by 中山龍 on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
